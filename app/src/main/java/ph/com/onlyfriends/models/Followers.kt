5d2d3dc23a2c4d858a64e6e75c73bc00b0531e2f
Lemsic <lemsicode@gmail.com>
2021-08-27 19:10:25 +0800

    Beta 3.6 - Deleted Followers

--- app/src/main/java/ph/com/onlyfriends/models/Followers.kt
+++
@@ -1,6 +0,0 @@
-package ph.com.onlyfriends.models
-
-open class Followers {
-
-    var nFollowers : Int = 0
-}
--- app/src/main/java/ph/com/onlyfriends/models/Friend.kt
+++ app/src/main/java/ph/com/onlyfriends/models/Friend.kt
@@ -5,5 +5,4 @@ open class Friend(_email: String = "", _name: String = "", _handle: String = "")
     var email: String = _email
     var name: String = _name
     var handle: String = "@$_handle"
-    var followers = Followers()
 }
