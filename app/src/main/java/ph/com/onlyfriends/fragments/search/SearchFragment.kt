373c4b7cf563de036daa26fb23bd9b01a5875d20
cyril-deguzman <cyrilethan.deguzman@gmail.com>
2021-09-08 18:49:51 +0800

    Beta 3.7 Search Function, Search bar now useable

--- app/src/main/java/ph/com/onlyfriends/fragments/search/SearchFragment.kt
+++ app/src/main/java/ph/com/onlyfriends/fragments/search/SearchFragment.kt
@@ -1,10 +1,14 @@
 package ph.com.onlyfriends.fragments.search
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import android.widget.SearchView.OnQueryTextListener
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.firebase.ui.database.FirebaseRecyclerAdapter
@@ -21,6 +25,7 @@ class SearchFragment : Fragment() {
     private lateinit var rvUserSearchList: RecyclerView
     private lateinit var lmUserSearchManager: RecyclerView.LayoutManager
     private lateinit var dbFriendDatabase: DatabaseReference
+    private lateinit var svSearchBar: SearchView
 
     override fun onCreateView (
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,22 +35,39 @@ class SearchFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_search, container, false)
 
         dbFriendDatabase = FirebaseDatabase.getInstance().getReference(Collections.Friends.name)
-        initRecyclerView(view)
+        initComponents(view)
 
         return view
     }
 
-    private fun initRecyclerView(view: View) {
+    private fun initComponents(view: View) {
+        initRecyclerView(view)
 
+        this.svSearchBar = view.findViewById(R.id.sv_search_bar)
+
+        this.svSearchBar.setOnQueryTextListener(object : OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Log.i(TAG,svSearchBar.query.toString())
+                rvUserSearchList.adapter = initFirebaseAdapter(svSearchBar.query.toString())
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return true
+            }
+        })
+    }
+
+    private fun initRecyclerView(view: View) {
         this.rvUserSearchList = view.findViewById(R.id.rv_search_users)
         this.lmUserSearchManager = LinearLayoutManager(view.context, LinearLayoutManager.VERTICAL, false)
         this.rvUserSearchList.layoutManager = this.lmUserSearchManager
-
-        this.rvUserSearchList.adapter = initFirebaseAdapter()
     }
 
-    private fun initFirebaseAdapter(): FirebaseRecyclerAdapter<Friend, SearchViewHolder> {
+    private fun initFirebaseAdapter(searchQuery: String): FirebaseRecyclerAdapter<Friend, SearchViewHolder> {
         val firebaseSearchQuery: Query = dbFriendDatabase.orderByChild("name")
+            .startAt(searchQuery)
+            .endAt(searchQuery + "\uf8ff")
 
         val options = FirebaseRecyclerOptions.Builder<Friend>()
             .setQuery(firebaseSearchQuery, Friend::class.java)
