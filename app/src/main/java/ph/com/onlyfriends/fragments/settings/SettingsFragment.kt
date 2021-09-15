1ab7b03451b0d73ebcb3746893f3cfab8620e048
Lemsic <lemsicode@gmail.com>
2021-09-15 19:49:29 +0800

    Beta 4.2 - Settings Progress

--- app/src/main/java/ph/com/onlyfriends/fragments/settings/SettingsFragment.kt
+++ app/src/main/java/ph/com/onlyfriends/fragments/settings/SettingsFragment.kt
@@ -6,12 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import com.google.firebase.auth.FirebaseAuth
+import android.widget.ImageView
+import android.widget.TextView
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import ph.com.onlyfriends.R
 
 class SettingsFragment : Fragment() {
+
+    private lateinit var myName: TextView
+    private lateinit var myHandle: TextView
+    private lateinit var myPFP: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,11 +26,23 @@ class SettingsFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_settings, container, false)
 
         initializeComponents(view)
+        initializeProfile()
 
         return view
     }
 
+    private fun initializeProfile() {
+
+        myPFP.setImageResource(R.drawable.ic_default_user)
+
+
+    }
+
     private fun initializeComponents(view: View) {
+
+        myName = view.findViewById(R.id.tv_my_name)
+        myHandle = view.findViewById(R.id.tv_my_handle)
+        myPFP = view.findViewById(R.id.iv_my_pfp)
 
         //Sign Out Button
         view.findViewById<Button>(R.id.btn_sign_out).setOnClickListener {
