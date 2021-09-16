4bdfd76bc6bb97df1541eff90cdd0d8f2b73e6f2
Lemsic <lemsicode@gmail.com>
2021-09-16 21:52:30 +0800

    Beta 6.3 - Added a lot.

--- app/src/main/java/ph/com/onlyfriends/fragments/settings/SettingsFragment.kt
+++ app/src/main/java/ph/com/onlyfriends/fragments/settings/SettingsFragment.kt
@@ -1,5 +1,6 @@
 package ph.com.onlyfriends.fragments.settings
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -17,6 +18,9 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.ktx.Firebase
+import org.w3c.dom.Text
+import ph.com.onlyfriends.AddPostActivity
+import ph.com.onlyfriends.EditProfileActivity
 import ph.com.onlyfriends.R
 import ph.com.onlyfriends.models.Collections
 
@@ -24,7 +28,12 @@ class SettingsFragment : Fragment() {
 
     private lateinit var myName: TextView
     private lateinit var myHandle: TextView
+    private lateinit var myFollowersCount: TextView
+    private lateinit var myBio: TextView
+
     private lateinit var myPFP: ImageView
+    private lateinit var editButton: Button
+
     private lateinit var user: FirebaseUser
     private lateinit var db: FirebaseDatabase
 
@@ -36,12 +45,12 @@ class SettingsFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_settings, container, false)
 
         initializeComponents(view)
-        initializeProfile()
+        initializeProfile(view)
 
         return view
     }
 
-    private fun initializeProfile() {
+    private fun initializeProfile(view: View) {
 
         myPFP.setImageResource(R.drawable.ic_default_user)
 
@@ -51,6 +60,14 @@ class SettingsFragment : Fragment() {
                     if (friend.key == user.uid) {
                         myName.text = friend.child("name").value.toString()
                         myHandle.text = friend.child("handle").value.toString()
+                        myFollowersCount.text = friend.child("numFollowers").value.toString()
+
+                        val bio: String = friend.child("bio").value.toString()
+                        if(bio.isNotEmpty()) {
+                            myBio.text = bio
+                        }
+                        else
+                            myBio.text = view.resources.getString(R.string.bio_empty)
                     }
                 }
             }
@@ -67,12 +84,21 @@ class SettingsFragment : Fragment() {
         db = FirebaseDatabase.getInstance()
         myName = view.findViewById(R.id.tv_my_name)
         myHandle = view.findViewById(R.id.tv_profile_handle)
+        myFollowersCount = view.findViewById(R.id.tv_followers)
+        myBio = view.findViewById(R.id.tv_settings_bio)
+
         myPFP = view.findViewById(R.id.iv_profile_pfp)
+        editButton = view.findViewById(R.id.btn_edit_profile)
 
         //Sign Out Button
         view.findViewById<Button>(R.id.btn_sign_out).setOnClickListener {
             Firebase.auth.signOut()
             activity?.finish()!!
+        }
+
+        editButton.setOnClickListener {
+            val intent = Intent(activity, EditProfileActivity::class.java)
+            activity?.startActivity(intent)
         }
     }
 }
