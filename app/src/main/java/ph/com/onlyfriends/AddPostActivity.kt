6a40819544c231f47a68d76863c239a571df65d6
Lemsic <lemsicode@gmail.com>
2021-09-16 12:23:26 +0800

    Beta 5.1 - Added a lot.

--- app/src/main/java/ph/com/onlyfriends/AddPostActivity.kt
+++ app/src/main/java/ph/com/onlyfriends/AddPostActivity.kt
@@ -2,18 +2,18 @@ package ph.com.onlyfriends
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import android.widget.Button
 
 class AddPostActivity : AppCompatActivity() {
 
-    private lateinit var fab: FloatingActionButton
+    private lateinit var btnCancel: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_post)
 
-        fab = findViewById(R.id.btn_post_cancel)
-        fab.setOnClickListener {
+        btnCancel = findViewById(R.id.btn_post_cancel)
+        btnCancel.setOnClickListener {
             finish() //go back in post or closes the activity
         }
     }
