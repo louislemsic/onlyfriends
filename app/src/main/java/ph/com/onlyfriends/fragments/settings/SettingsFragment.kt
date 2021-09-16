a05f3e16ad6391b49fceaa1e4502aebb689f466a
Lemsic <lemsicode@gmail.com>
2021-09-16 21:53:09 +0800

    Beta 6.3 - Added a lot.

--- app/src/main/java/ph/com/onlyfriends/fragments/settings/SettingsFragment.kt
+++ app/src/main/java/ph/com/onlyfriends/fragments/settings/SettingsFragment.kt
@@ -18,8 +18,6 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.ktx.Firebase
-import org.w3c.dom.Text
-import ph.com.onlyfriends.AddPostActivity
 import ph.com.onlyfriends.EditProfileActivity
 import ph.com.onlyfriends.R
 import ph.com.onlyfriends.models.Collections
