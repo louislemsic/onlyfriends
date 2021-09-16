dbe64a94e12be344496198f52908f815916ee7ba
cyril-deguzman <cyrilethan.deguzman@gmail.com>
2021-09-16 19:36:06 +0800

    Beta 6.2 Updated Friend model

--- app/src/main/java/ph/com/onlyfriends/models/Friend.kt
+++ app/src/main/java/ph/com/onlyfriends/models/Friend.kt
@@ -7,6 +7,9 @@ open class Friend(_email: String = "", _name: String = "", _handle: String = "")
     var handle: String = "@$_handle"
     var bio: String = ""
     var following: MutableMap<String, String> = HashMap()
-    var numFollowers: Int = 0
+    var numFollowers: Int = 1
 
+    init {
+        following[handle] = name
+    }
 }
